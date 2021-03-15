6bd164b310d47abb047b395991151baee6a6659f
co3oing <co3oing@naver.com>
2021-03-15 10:44:11 +0900

    BountyList - TableView

---
+++ Fast campus/BountyList/BountyList/BountyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BountyViewController.swift
+//  BountyList
+//
+//  Created by Lucia on 2021/03/15.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Fast campus/BountyList/BountyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BountyList
+//
+//  Created by Lucia on 2021/03/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
