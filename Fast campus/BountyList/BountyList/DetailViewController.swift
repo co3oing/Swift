0ce76914b9cce93f95103261520cba6626d0974f
co3oing <co3oing@naver.com>
2021-03-16 22:45:47 +0900

    DetailViewController MVVM

--- Fast campus/BountyList/BountyList/DetailViewController.swift
+++ Fast campus/BountyList/BountyList/DetailViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    // MVVM
+    
+    // Model
+    // - BountyInfo
+    // > BountyInfo 만들자
+    
+    // View
+    // - imgView, nameLabel, bountyLabel
+    // > view들은 viewModel을 통해서 구성되기 ?
+    
+    // ViewModel
+    // - DetailViewModel
+    // > 뷰레이어에서 필요한 메서드 만들기
+    // > 모델 가지고 있기. BountyInfo들
+    
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
