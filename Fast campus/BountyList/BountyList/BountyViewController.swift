6c0bef105b0dc04ba4d913df517aa8ed21b6ae33
co3oing <co3oing@naver.com>
2021-03-22 21:57:44 +0900

    line

--- Fast campus/BountyList/BountyList/BountyViewController.swift
+++ Fast campus/BountyList/BountyList/BountyViewController.swift
@@ -71,35 +71,13 @@ class BountyViewController: UIViewController, UICollectionViewDataSource, UIColl
     // UICollectionViewDelegateFlowLayout
     // cell size를 계산 (목표 : 다양한 디바이스에서 일관적인 디자인을 보여주기 위해)
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
         let itemSpacing: CGFloat = 10
         let textAreaHeight: CGFloat = 65
         
         let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2
         let height: CGFloat = width * 10/7 + textAreaHeight
-        
-        
         return CGSize(width: width, height: height)
     }
-    
-//    // UITableViewDataSource
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return viewModel.numOfBountyInfoList
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
-//            return UITableViewCell()
-//        }
-//        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
-//        cell.update(info: bountyInfo)
-//        return cell
-//    }
-//    // UITableViewDelegate
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print("--> \(indexPath.row)")
-//        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
-//    }
 }
 
 class BountyViewModel {
