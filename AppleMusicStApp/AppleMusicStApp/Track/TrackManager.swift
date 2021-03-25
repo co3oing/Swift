eec2a02ff5069702de5afce15dfff925f9f3515b
co3oing <co3oing@naver.com>
2021-03-25 09:25:19 +0900

    TrackManager

--- AppleMusicStApp/AppleMusicStApp/Track/TrackManager.swift
+++ AppleMusicStApp/AppleMusicStApp/Track/TrackManager.swift
@@ -27,34 +27,34 @@ class TrackManager {
     func loadTracks() -> [AVPlayerItem] {
         // 파일들 읽어서 AVPlayerItem 만들기
         let urls = Bundle.main.urls(forResourcesWithExtension: "mp3", subdirectory: nil) ?? []
-        
-//        var items: [AVPlayerItem] = []
-//        for url in urls {
-//            let item = AVPlayerItem(url: url)
-//            items.append(item)
-//        }
-        
-//        let items = urls.map { url in
-//            return AVPlayerItem(url: url)
-//        }
-        
         let items = urls.map { AVPlayerItem(url: $0) }
-        
         return items
     }
     
     // TODO: 인덱스에 맞는 트랙 로드하기
     func track(at index: Int) -> Track? {
-        return nil
+        let playerItem = tracks[index]
+        return playerItem.convertToTrack()
     }
 
     // TODO: 앨범 로딩메소드 구현
     func loadAlbums(tracks: [AVPlayerItem]) -> [Album] {
-        return []
+        let trackList: [Track]  = tracks.compactMap { $0.convertToTrack() }
+        let albumDics = Dictionary(grouping: trackList, by: { (track) in track.albumName })
+        var albums: [Album] = []
+        
+        for (key, value) in albumDics {
+            let title = key
+            let tracks = value
+            let album = Album(title: title, tracks: tracks)
+            albums.append(album)
+        }
+        
+        return albums
     }
 
     // TODO: 오늘의 트랙 랜덤으로 선책
     func loadOtherTodaysTrack() {
-        
+        self.todaysTrack = self.tracks.randomElement()
     }
 }
