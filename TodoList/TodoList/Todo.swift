ddb3ff20639de38927c4c7190d08f52c298073eb
co3oing <co3oing@naver.com>
2021-04-03 15:33:08 +0900

    todoManager

--- TodoList/TodoList/Todo.swift
+++ TodoList/TodoList/Todo.swift
@@ -31,29 +31,39 @@ struct Todo: Codable, Equatable {
 
 class TodoManager {
     
-    static let shared = TodoManager()
+    static let shared = TodoManager() // singleton
     
     static var lastId: Int = 0
     
     var todos: [Todo] = []
     
     func createTodo(detail: String, isToday: Bool) -> Todo {
-        //TODO: create로직 추가
-        return Todo(id: 1, isDone: false, detail: "2", isToday: true)
+        // [v] TODO: create로직 추가
+        let nextId = TodoManager.lastId + 1
+        TodoManager.lastId = nextId
+        return Todo(id: nextId, isDone: false, detail: detail, isToday: isToday)
     }
     
     func addTodo(_ todo: Todo) {
-        //TODO: add로직 추가
+        // [v] TODO: add로직 추가
+        todos.append(todo)
+        saveTodo()
     }
     
     func deleteTodo(_ todo: Todo) {
-        //TODO: delete 로직 추가
-        
+        // [v] TODO: delete 로직 추가
+        todos = todos.filter{ $0.id != todo.id }
+//        if let index = todos.firstIndex(of: todo) {
+//            todos.remove(at: index)
+//        }
+        saveTodo()
     }
     
     func updateTodo(_ todo: Todo) {
-        //TODO: updatee 로직 추가
-        
+        // [v] TODO: updatee 로직 추가
+        guard let index = todos.firstIndex(of: todo) else { return }
+        todos[index].update(isDone: todo.isDone, detail: todo.detail, isToday: todo.isToday)
+        saveTodo()
     }
     
     func saveTodo() {
