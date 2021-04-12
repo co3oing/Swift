b1bc09503f43173e27e6c214af676dc3e20ffbf2
co3oing <co3oing@naver.com>
2021-04-12 23:21:23 +0900

    URLSession, URL Components

--- Fast campus/4_Networking/3_URLSession - Basic.playground/Contents.swift
+++ Fast campus/4_Networking/3_URLSession - Basic.playground/Contents.swift
@@ -1,3 +1,47 @@
 import UIKit
 
-var str = "Hello, playground"
+// URLSession
+
+// 1. URLSessionConfiguration
+// 2. URLSession
+// 3. URLSessionTask 를 이용해서 서버와 네트워킹
+
+// URLSessionTask
+
+// - dataTask
+// - uploadTask
+// - downloadTask
+
+
+let config = URLSessionConfiguration.default
+let session = URLSession(configuration: config)
+
+// URL
+// URL Components
+var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
+let mediaQuery = URLQueryItem(name: "media", value: "music")
+let entityQuery = URLQueryItem(name: "entity", value: "song")
+let termQuery = URLQueryItem(name: "term", value: "aespa")
+urlComponents.queryItems?.append(mediaQuery)
+urlComponents.queryItems?.append(entityQuery)
+urlComponents.queryItems?.append(termQuery)
+let requestURL = urlComponents.url!
+
+let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
+    guard  error == nil else { return }
+    guard let statusCode = (response as? HTTPURLResponse)?.statusCode else { return }
+    
+    let successRange = 200..<300
+    
+    guard successRange.contains(statusCode) else {
+        // handle response error
+        return
+    }
+    
+    guard let resultData = data else { return }
+    let resultString = String(data: resultData, encoding: .utf8)
+    
+    print("---> result : \(resultString!)")
+}
+
+dataTask.resume()
