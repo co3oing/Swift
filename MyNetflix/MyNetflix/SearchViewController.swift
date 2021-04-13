d6c97815f102da5b5bdd72f8869f934bbaac9647
co3oing <co3oing@naver.com>
2021-04-14 00:07:48 +0900

    키보드 내리기

--- MyNetflix/MyNetflix/SearchViewController.swift
+++ MyNetflix/MyNetflix/SearchViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    private func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // 키보드가 올라와 있을 때, 내려가게 처리
+        dismissKeyboard()
+        
+        // 검색어가 있는지
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
+        
+        // 네트워킹을 통한 검색
+        print("--> 검색어: \(searchBar.text)")
+    }
 }
