37ef3a6fe96c189579bb5df34162bb2ad20ce250
co3oing <co3oing@naver.com>
2021-04-16 13:23:23 +0900

    플레이어뷰 UI 구성

--- MyNetflix/MyNetflix/PlayerViewController.swift
+++ MyNetflix/MyNetflix/PlayerViewController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayerViewController: UIViewController {
+    
+    @IBOutlet weak var playerView: PlayerView!
+    @IBOutlet weak var controlView: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    
+    let player = AVPlayer()
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscapeRight
@@ -18,15 +25,18 @@ class PlayerViewController: UIViewController {
         super.viewDidLoad()
     }
     
-
+    @IBAction func togglePlaybutton(_ sender: Any) {
+        playButton.isSelected = !playButton.isSelected
+    }
+    
     @IBAction func closeButtonTapped(_ sender: Any) {
         dismiss(animated: false, completion: nil)
     }
 }
 
-//extension AVPlayer {
-//    var isPlaying: Bool {
-//        guard self.currentItem != nil else { return false }
-//        return self.rate != 0
-//    }
-//}
+extension AVPlayer {
+    var isPlaying: Bool {
+        guard self.currentItem != nil else { return false }
+        return self.rate != 0
+    }
+}
